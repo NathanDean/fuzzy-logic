35e87f1350759902b17f32c6f5f38c62ec6bddf5
Nathan Dean <nathandean@hotmail.co.uk>
2025-03-16 23:46:17 +0000

    Fix dropdown nav centring issue

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -105,15 +105,51 @@ export default function Header(){
 
       {isMenuVisible && (
 
-        <div className = {`sm:hidden absolute w-full h-screen bg-white p-6 transition-opacity duration-300 ease-in-out z-20 flex items-center justify-center
+        <div className = {`sm:hidden fixed inset-0 w-full h-screen bg-white p-6 transition-opacity duration-300 ease-in-out z-0 flex justify-center items-center
         
         ${isMenuOpaque ? "opacity-100" : "opacity-0"}
         
         `}>
 
-          <nav className = {`${outfit.className} flex flex-col justify-around h-1/2 space-y-6 text-xl tracking-wider`}>
+          <div className = "fixed top-0 left-0 right-0 px-6 py-4">
+
+            <div className = "flex justify-between items-center">
+            
+              <Link href = "/" className = {`${oi.className} text-2xl`}  onClick = {() => setIsMenuOpen(false)}>fuzzy logic</Link>
+
+              <button className = "sm:hidden" onClick = {toggleMenu} aria-label = "Toggle menu">
+
+                <svg className = "w-6 h-6" fill = "none" stroke = "currentColor" viewBox = "0 0 24 24" xmlns = "http://www.w3.org/2000/svg">
+                
+                  {isMenuOpen ? (
+                      // X icon when menu is open
+                      <path 
+                        strokeLinecap="round" 
+                        strokeLinejoin="round" 
+                        strokeWidth={2} 
+                        d="M6 18L18 6M6 6l12 12" 
+                      />
+                    ) : (
+                      // Hamburger icon when menu is closed
+                      <path 
+                        strokeLinecap="round" 
+                        strokeLinejoin="round" 
+                        strokeWidth={2} 
+                        d="M4 6h16M4 12h16M4 18h16" 
+                      />
+                    )}
+                
+                </svg>
+
+              </button>
+
+            </div>
+
+          </div>
+
+          <nav className = {`${outfit.className} flex flex-col justify-center h-1/2 space-y-6 text-xl tracking-wider`}>
                     
-            <Link href = "/workshops" className = "hover:text-blue-500 transitio text-center text-3xl" onClick = {() => setIsMenuOpen(false)}>Workshops</Link>
+            <Link href = "/workshops" className = "hover:text-blue-500 transition text-center text-3xl" onClick = {() => setIsMenuOpen(false)}>Workshops</Link>
 
             <Link href = "/teachers" className = "hover:text-blue-500 transition text-center text-3xl" onClick = {() => setIsMenuOpen(false)}>Teachers</Link>
 
