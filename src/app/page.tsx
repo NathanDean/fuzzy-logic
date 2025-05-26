7025dcc1e2d63e864ea26eb9fc98b90fd836bdca
Nathan Dean <nathandean@hotmail.co.uk>
2025-05-26 21:32:27 +0100

    Adjust home page spacing to account for footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   
   return (
 
-    <div className = "flex flex-col h-full items-center justify-center">
+    <div className = "flex flex-col h-full items-center justify-center pt-6">
 
       <h1 className = "logo main-logo text-center px-1">
         
