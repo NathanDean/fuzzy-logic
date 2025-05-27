862b993d0434f5a1855418192b44dfb426569e57
Nathan Dean <nathandean@hotmail.co.uk>
2025-05-27 12:05:21 +0100

    Update About copy

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,10 +5,10 @@ export default function About(){
         <div className = "sm:w-xl lg:w-3xl xl:w-4xl space-y-2 large-text">
 
             <p>Fuzzy Logic is an improv company.</p>
+ 
+            <p>We run workshops designed to help improvisers find their artistic voice and develop the technical skills to express it more clearly.</p>
 
-            <p>We run a small and carefully curated selection of workshops designed to help improvisers find their artistic voice and develop the technical skills to express it more clearly.</p>
-
-            <p>We aim to explore the ways improv can enhance and learn from other artforms, and experiment with new ideas of what improv can do and be.</p>
+            <p>We aim to explore the ways improv can give to and learn from other artforms, and experiment with new ideas of what improv can do and be.</p>
 
         </div>
 
