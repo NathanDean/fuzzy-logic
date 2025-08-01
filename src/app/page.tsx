c7e4044fa730da5a5dc224e2630ab768b1fde730
Nathan Dean <nathandean@hotmail.co.uk>
2025-08-01 22:16:57 +0100

    Change spacing on homepage logo to account for spacing changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="flex flex-col h-full items-center justify-center pt-6">
+    <div className="flex flex-col h-full items-center justify-center">
       <h1 className="logo main-logo text-center px-1">fuzzy logic</h1>
 
       <h2 className="subheading">an improv company</h2>
