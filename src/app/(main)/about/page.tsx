7cca1cc0f3467fada747c59b022b24c5f5ff8ea6
Nathan Dean <nathandean@hotmail.co.uk>
2026-01-12 17:17:01 +0000

    Update About page copy

--- src/app/(main)/about/page.tsx
+++ src/app/(main)/about/page.tsx
@@ -35,21 +35,22 @@ export default async function About() {
   return (
     <div className="-mt-15 sm:-mt-30 -mb-13 sm:-mb-16">
       <section className="min-h-screen flex flex-col justify-center items-start space-y-2">
-        <Text>{`Fuzzy Logic is an improv company.`}</Text>
-
+        <Text>Fuzzy Logic is an improv company.</Text>
         <Text>
-          {`We run workshops designed to help improvisers find their artistic voice and develop the technical skills to express it more clearly.`}
+          We run workshops targeting specific technical skills that make up the
+          craft of improv.
         </Text>
-
         <Text>
-          {`Our work explores the ways improv can give to and learn from other artforms, and experiments with new ideas of what improv can do and be.`}
+          Our students are improvisers who are actively performing, and are
+          ambitious about improving their skills and exploring the artistic
+          potential of improv.
         </Text>
         <div className="w-full flex flex-row items-stretch justify-center space-x-2">
           <Button href="#people">
-            <Text as="span">{`Who is Fuzzy Logic?`}</Text>
+            <Text as="span">{`Who we are`}</Text>
           </Button>
           <Button href="#story">
-            <Text as="span">{`What's our story?`}</Text>
+            <Text as="span">{`Our story`}</Text>
           </Button>
         </div>
       </section>
@@ -78,7 +79,7 @@ export default async function About() {
         className="min-h-screen flex flex-col justify-center items-start space-y-2"
       >
         <Text>
-          {`Fuzzy Logic was founded in 2025 by Nathan Dean, following a decade's experience in the world of artist and artform development at Arts Council England and talent agency Avalon.`}
+          {`Fuzzy Logic was founded by Nathan Dean, following a decade's experience in the world of artist and artform development at Arts Council England and talent agency Avalon.`}
         </Text>
         <Text>
           {`We ran our first workshops in July 2025. We also produce the sketch series scenesAboutLeaving and the twoprov show A Very Capable Boy, with more shows to come in future.`}
